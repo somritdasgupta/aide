2dc7547473e42d64776e71deec6595903af9e918
Somrit Dasgupta <somritdasgupta7@gmail.com>
2024-07-26 19:04:17 +0530

    added docs for aiDe

---
+++ docs/theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>aiDe⚡docs</span>,
+  project: {
+    link: 'https://github.com/somritdasgupta/aide',
+  },
+  docsRepositoryBase: 'https://github.com/somritdasgupta/aide/tree/main/docs',
+  footer: {
+    text: 'aiDe by Somrit Dasgupta',
+  },
+}
+
+export default config
